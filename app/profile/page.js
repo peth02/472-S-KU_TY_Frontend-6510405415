d0db6b3740fb3765f88ffeb755bfa2a0d97c3755
naphatSUA <naphat.sua@ku.th>
2024-11-02 23:11:57 +0700

    Create Profile UI Page

--- app/profile/page.js
+++ app/profile/page.js
@@ -3,28 +3,35 @@ import styles from "./page.module.css";
 
 export default function Profile() {
   return (
-    <div className={styles.page}>
-      <div className={styles["event-detail-container"]}>
-        <div className={styles["event-detail-picture"]}>
-          <Image
-            src="/images/default-event-picture.png"
-            layout="responsive"
-            width={100}
-            height={100}
-            alt="Event picture"
-          />
+    <div className={styles.profileContainer}>
+      {/* Profile Header */}
+      <div className={styles.profileHeader}>
+        {/* Profile Avatar */}
+        <div className={styles.profileAvatar}>
+          <span className={styles.avatarText}>S</span>
         </div>
 
-        <div className={styles["event-detail"]}></div>
+        {/* Profile Info */}
+        <div className={styles.profileInfo}>
+          <div className={styles.profileHeaderContent}>
+            <div className={styles.profileNameRow}>
+              <h1 className={styles.profileName}>Somchai Marison</h1>
+            </div>
+            <div className={styles.profileButtonRow}>
+              <button className={styles.profileGreyButton}>แก้ไขโปรไฟล์</button>
+              <button className={styles.profileGreyButton}>
+                แก้ไขรายละเอียด
+              </button>
+            </div>
+          </div>
+          <p className={styles.profileFaculty}>
+            คณะวิทยาศาสตร์ สาขาวิทยาการคอมพิวเตอร์ ปี 1
+          </p>
+          <p className={styles.profileEmail}>somchai@gmail.com</p>
+          <p className={styles.profileDetails}>@somchaiMS</p>
+        </div>
       </div>
-
-      <div className={styles["event-description"]}>
-        <p style={{ fontWeight: "bold", fontSize: 40 }}>รายละเอียด</p>
-        <p style={{ fontSize: 22 }}>
-          หาเพื่อนไปเล่นบอร์ดเกม จอย ๆ กัน ห้องเล่นบอร์ดเกม ที่หอสมุด
-          มาเจอกันนะ..
-        </p>
-      </div>
+      <hr className={styles.profileDivider} />
     </div>
   );
 }
