8211317df853bc188f9c73111a0ef16651715cc5
naphatSUA <naphat.sua@ku.th>
2024-11-03 15:34:13 +0700

    Fetch DetailEvent Page

--- app/all-events/event/page.js
+++ app/all-events/event/page.js
@@ -1,7 +1,35 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
+import { fetchEventDetails } from "../../apis/event/api";
 
 export default function Event() {
+  const [eventDetails, setEventDetails] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const eventId = "3bdc045a-c95d-4954-a590-cfe1d10e51bd"; // Replace with dynamic event ID if needed
+    fetchEventDetails(eventId)
+      .then((response) => {
+        console.log(response["data"]);
+        setEventDetails(response["data"]);
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error);
+      });
+  }, []);
+
+  if (error) {
+    return <div>Error loading event details: {error.message}</div>;
+  }
+
+  if (!eventDetails) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <div className={styles["event-detail-container"]}>
@@ -15,74 +43,95 @@ export default function Event() {
           />
         </div>
 
-        <div className={styles['event-detail']}>
+        <div className={styles["event-detail"]}>
           <div>
-            <p className={styles['event-name-text']}>บอร์ดเกมมหาสนุก</p>
+            {eventDetails && eventDetails["name"] ? (
+              <p className={styles["event-name-text"]}>
+                {eventDetails["name"]}
+              </p>
+            ) : (
+              <p>Event name not available</p>
+            )}
           </div>
           <div>
-            <p style={{fontSize: 24, marginBottom:30}}>โดย <span style={{fontWeight: 'bold'}}>หมูเด้ง ช่วยหมูเด้งด้วย</span></p>
+            <p style={{ fontSize: 24, marginBottom: 30 }}>
+              โดย{" "}
+              <span style={{ fontWeight: "bold" }}>
+                {eventDetails &&
+                eventDetails["createdBy"] &&
+                eventDetails["createdBy"]["firstName"]
+                  ? `${eventDetails["createdBy"]["firstName"]} ${eventDetails["createdBy"]["lastName"]}`
+                  : "Anonymous"}
+              </span>
+            </p>
           </div>
           <div>
-            <div className={styles['event-detail-date-location']}>
-              <div className={styles['event-detail-date']}>
+            <div className={styles["event-detail-date-location"]}>
+              <div className={styles["event-detail-date"]}>
                 <Image
                   src="/images/calendar-icon.png"
                   width={18}
                   height={20}
                   alt="Calendar icon"
                 />
-                <p className={styles['event-detail-text']}>2 Nov 2024</p>
+                <p className={styles["event-detail-text"]}>
+                  {eventDetails && eventDetails["startDate"]
+                    ? `${eventDetails["startDate"]}`
+                    : "ไม่กำหนดวันที่"}
+                </p>
               </div>
-              <div className={styles['event-detail-location']}>
+              <div className={styles["event-detail-location"]}>
                 <Image
                   src="/images/location-icon.png"
                   width={14}
                   height={20}
                   alt="Location icon"
                 />
-                <p className={styles['event-detail-text']}>สำนักหอสมุดเกษตรศาสตร์</p>
+                <p className={styles["event-detail-text"]}>
+                  {eventDetails && eventDetails["location"]
+                    ? `${eventDetails["location"]}`
+                    : "ไม่กำหนดสถานที่"}
+                </p>
               </div>
             </div>
-            <div className={styles['event-detail-time']}>
+            <div className={styles["event-detail-time"]}>
               <Image
-                  src="/images/clock-icon.png"
-                  width={20}
-                  height={20}
-                  alt="Clock icon"
-                />
-                <p className={styles['event-detail-text']}>09 : 00 น.</p>
+                src="/images/clock-icon.png"
+                width={20}
+                height={20}
+                alt="Clock icon"
+              />
+              <p className={styles["event-detail-text"]}>
+                {eventDetails && eventDetails["startDate"]
+                  ? `${eventDetails["startDate"]}`
+                  : "ไม่กำหนดเวลา"}
+              </p>
             </div>
           </div>
-          <div className={styles['event-participants']}>
-            <div>
-              <Image
-                  src="/images/default-participants-picture.png"
-                  width={46}
-                  height={45}
-                  alt="Participants picture"
-                />
-            </div>
-            <div>
-              <div className={styles['number-of-participants-container']}>
-                  <div>
-                    <Image
-                      src="/images/participants-icon.png"
-                      width={26}
-                      height={23}
-                      alt="Participants icon"
-                    />
-                  </div>
-                  <div>
-                    <p style={{fontSize:22}}>10</p>
-                  </div>
+          <div className={styles["eventTagRow"]}>
+            <div className={styles["event-tag"]}>Board Game</div>
+            <div className={styles["event-participants"]}>
+              <div className={styles["number-of-participants-container"]}>
+                <div>
+                  <Image
+                    src="/images/participants-icon.png"
+                    width={26}
+                    height={23}
+                    alt="Participants icon"
+                  />
+                </div>
+                <div>
+                  <p style={{ fontSize: 22 }}>
+                    {eventDetails && eventDetails["attendeeCount"]
+                      ? `${eventDetails["attendeeCount"]}`
+                      : "ไม่มีข้อมูล"}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={styles['event-tag']}>
-            Board Game
           </div>
           <div>
-            <button className={styles['join-event-button']}>เข้าร่วม</button>
+            <button className={styles["join-event-button"]}>เข้าร่วม</button>
           </div>
         </div>
       </div>
@@ -90,8 +139,9 @@ export default function Event() {
       <div className={styles["event-description"]}>
         <p style={{ fontWeight: "bold", fontSize: 40 }}>รายละเอียด</p>
         <p style={{ fontSize: 22 }}>
-          หาเพื่อนไปเล่นบอร์ดเกม จอย ๆ กัน ห้องเล่นบอร์ดเกม ที่หอสมุด
-          มาเจอกันนะ..
+          {eventDetails && eventDetails["description"]
+            ? `${eventDetails["description"]}`
+            : "ไม่มีรายละเอียด"}
         </p>
       </div>
     </div>
