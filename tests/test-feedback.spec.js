b2a3759a99a901ecdd04a1baed539c21d6eea679
kriengkai <kriengkai.s@ku.th>
2025-03-27 22:34:23 +0700

    Add test feedback

--- tests/test-feedback.spec.js
+++ tests/test-feedback.spec.js
@@ -1,45 +1,66 @@
-// import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-// test('test-filter-location', async ({ page }) => {
-//   await page.goto('/');
-//   await page.getByRole('textbox', { name: 'Input Field' }).click();
-//   await page.getByRole('textbox', { name: 'Input Field' }).fill('b6510405351');
-//   await page.getByRole('textbox', { name: 'Input Field' }).press('Tab');
-//   await page.getByRole('textbox', { name: 'Input Password' }).fill('210746_Ttt');
-//   await page.getByRole('button', { name: 'เข้าสู่ระบบ' }).click();
-//   await page.locator('select[name="location"]').selectOption('11');
-//   await page.locator('div').filter({ hasText: '1โดย Kriengkai Seethong2025-' }).nth(3).click();
-//   const targetDiv = await page.locator('div').filter({ hasText: '1โดย Kriengkai Seethong2025-' }).nth(3);
-  
-//   // ตรวจสอบว่า element ที่มีข้อความตรงกับที่ต้องการมีอยู่
-//   await expect(targetDiv).toBeVisible(); 
-// });
+test('test-see-feedback', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('textbox', { name: 'Input Field' }).click();
+  await page.getByRole('textbox', { name: 'Input Field' }).fill('b6510405351');
+  await page.getByRole('textbox', { name: 'Input Field' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Input Password' }).fill('210746_Ttt');
+  await page.getByRole('button', { name: 'เข้าสู่ระบบ' }).click();
+  await page.locator('div').filter({ hasText: /^-1 รายละเอียด$/ }).getByRole('button').click();
+  await page.getByText('ไม่ระบุตัวตนjjjjjjjjjaaaaaaajajajjajajaj').click();
+  const targetElement = await page.getByText('ไม่ระบุตัวตนjjjjjjjjjaaaaaaajajajjajajaj');
+  await expect(targetElement).toBeVisible();
+});
 
-// test('test-filter-date', async ({ page }) => {
-//   await page.goto('/');
-//   await page.getByRole('textbox', { name: 'Input Field' }).click();
-//   await page.getByRole('textbox', { name: 'Input Field' }).fill('b6510405351');
-//   await page.getByRole('textbox', { name: 'Input Field' }).press('Tab');
-//   await page.getByRole('textbox', { name: 'Input Password' }).fill('210746_Ttt');
-//   await page.getByRole('button', { name: 'เข้าสู่ระบบ' }).click();
-//   await page.locator('select[name="date"]').selectOption('2025-03-07');
-//   await page.locator('div').filter({ hasText: 'หฟดฟหกดฟโดย Kriengkai' }).nth(3).click();
-//   const targetDiv = await page.locator('div').filter({ hasText: 'หฟดฟหกดฟโดย Kriengkai' }).nth(3);
+test('test-send-feedback-anonymously', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('textbox', { name: 'Input Field' }).click();
+  await page.getByRole('textbox', { name: 'Input Field' }).fill('b6510405351');
+  await page.getByRole('textbox', { name: 'Input Field' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Input Password' }).fill('210746_Ttt');
+  await page.getByRole('button', { name: 'เข้าสู่ระบบ' }).click();
+  await page.locator('div').filter({ hasText: /^-1 รายละเอียด$/ }).getByRole('button').click();
+  await page.getByRole('textbox', { name: 'เขียน Feedback' }).click();
+  await page.getByRole('textbox', { name: 'เขียน Feedback' }).fill('commetn feedback brrab arrasdra');
   
-//   // ตรวจสอบว่า element ที่มีข้อความตรงกับที่ต้องการมีอยู่
-//   await expect(targetDiv).toBeVisible(); 
-// });
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
 
-// test('test-filter-type', async ({ page }) => {
-//   await page.goto('/');
-//   await page.getByRole('textbox', { name: 'Input Field' }).fill('b6510405351');
-//   await page.getByRole('textbox', { name: 'Input Field' }).press('Tab');
-//   await page.getByRole('textbox', { name: 'Input Password' }).fill('210746_Ttt');
-//   await page.getByRole('button', { name: 'เข้าสู่ระบบ' }).click();
-//   await page.locator('select[name="type"]').selectOption('กดก');
-//   await page.locator('div').filter({ hasText: 'หฟดฟหกดฟโดย Kriengkai' }).nth(3).click();
-//   const targetDiv = await page.locator('div').filter({ hasText: 'หฟดฟหกดฟโดย Kriengkai' }).nth(3);
+  await page.getByRole('button', { name: 'ส่ง Feedback แบบไม่ระบุตัวตน' }).click();
   
-//   // ตรวจสอบว่า element ที่มีข้อความตรงกับที่ต้องการมีอยู่
-//   await expect(targetDiv).toBeVisible(); 
-// });
+  // ตรวจสอบว่า element ที่มีข้อความตรงกับที่ต้องการมีอยู่หรือไม่
+  const feedbackElement = await page.getByText('ไม่ระบุตัวตนcommetn feedback');
+  await expect(feedbackElement).toBeVisible();  // ตรวจสอบว่า element นั้นมีอยู่บนหน้า
+
+  // คลิกที่ element หากมันมีอยู่
+  await feedbackElement.click();
+});
+
+
+test('test-send-feedback', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('textbox', { name: 'Input Field' }).click();
+  await page.getByRole('textbox', { name: 'Input Field' }).fill('b6510405351');
+  await page.getByRole('textbox', { name: 'Input Field' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Input Password' }).fill('210746_Ttt');
+  await page.getByRole('button', { name: 'เข้าสู่ระบบ' }).click();
+  await page.locator('div').filter({ hasText: /^-1 รายละเอียด$/ }).getByRole('button').click();
+  await page.getByRole('textbox', { name: 'เขียน Feedback' }).click();
+  await page.getByRole('textbox', { name: 'เขียน Feedback' }).fill(';saldflaskdfjs;lfslfsadfjslksssfffsjdlksfjlksfjl');
+  page.once('dialog', dialog => {
+    console.log(`Dialog message: ${dialog.message()}`);
+    dialog.dismiss().catch(() => {});
+  });
+  await page.getByRole('button', { name: 'ส่ง Feedback', exact: true }).click();
+  
+  // ตรวจสอบว่า element ที่มีข้อความตรงกับที่ต้องการมีอยู่หรือไม่
+  const feedbackElement = await page.getByText('Kriengkai;saldflaskdfjs;');
+  await expect(feedbackElement).toBeVisible();  // ตรวจสอบว่า element นั้นมีอยู่บนหน้า
+
+  // คลิกที่ element หากมันมีอยู่
+  await feedbackElement.click();
+});
+
